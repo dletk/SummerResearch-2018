b872266e0e81b1a6d23017c4f9aa7953381f037f
dletk <duc.mtle@gmail.com>
2018-07-03 15:10:11 -0500

    Modify script to drop the last layer of the networkd

--- Facial_Recognition/NeuralNetwork/convertToTensorflow.py
+++ Facial_Recognition/NeuralNetwork/convertToTensorflow.py
@@ -5,9 +5,23 @@ from tensorflow.python.framework import graph_util
 from tensorflow.python.framework import graph_io
 import sys
 
+# Load the pretrained model in (.h5 file)
 model = keras.models.load_model(sys.argv[1])
+# Set the learning phase to 0 to able to convert and import to openCV
 K.set_learning_phase(0)
 
+# =================================================================
+# ============== Drop the last layer of the model =================
+#==================================================================
+# We are interested in the secon last layer of the model for 128 measurements
+model.pop()
+
+print(model.summary())
+
+
+# =================================================================
+# ============ Convert the model to tensorflow model ==============
+#==================================================================
 pred_node_names = [None]
 pred = [None]
 for i in range(1):
@@ -15,5 +29,6 @@ for i in range(1):
     pred[i] = tf.identity(model.outputs[i], name=pred_node_names[i])
 
 sess = K.get_session()
+# Convert to a constant graph
 constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), pred_node_names)
 graph_io.write_graph(constant_graph, ".", "model.pb", as_text=False)
--- Facial_Recognition/NeuralNetwork/tutorialOpenCVNN.cpp
+++ Facial_Recognition/NeuralNetwork/tutorialOpenCVNN.cpp
@@ -3,6 +3,7 @@
 
 #include<iostream>
 #include<stdio.h>
+#include<chrono>
 
 using namespace std;
 using namespace cv;
@@ -10,7 +11,7 @@ using namespace cv;
 int main(int argc, char** argv) {
 	String model = "./model.pb";
 	String modeltxt = "./model.pbtxt";
-	Mat image = imread("./example.jpg");
+	Mat image = imread(argv[1]);
 	
 	cvtColor(image, image, COLOR_BGR2GRAY);
 	
@@ -28,12 +29,23 @@ int main(int argc, char** argv) {
 		cout << "Loaded network successfully" << endl;
 	}
 	
+	auto startTime = chrono::system_clock::now();
+	
 	Mat inputBlob = dnn::blobFromImage(image);   //Convert Mat to dnn::Blob image batch
     net.setInput(inputBlob);        //set the network input
+    
+	auto loadedInput = chrono::system_clock::now();
 
 	cout << "Loaded input" << endl;
 	
     cout << net.forward() << endl;   
+    
+    auto endTime = chrono::system_clock::now();
+    chrono::duration<double> time = endTime - startTime;
+    chrono::duration<double> inputTime = loadedInput - startTime;
+    
+    cout << "Load input time: " << inputTime.count() << endl;
+    cout << "Total runtime time: " << time.count() << endl;
 	
 	return 0;
 }
